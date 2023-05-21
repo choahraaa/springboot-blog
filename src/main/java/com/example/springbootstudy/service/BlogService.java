3e2762ac7a976ecaa740240d129f1b7dcdde9783
choahra <ahar0729@gamil.com>
2023-05-21 10:38:06 +0900

    service add

---
+++ src/main/java/com/example/springbootstudy/service/BlogService.java
@@ -0,0 +1,19 @@
+package com.example.springbootstudy.service;
+
+import com.example.springbootstudy.dto.AddArticleRequest;
+import com.example.springbootstudy.entity.Article;
+import com.example.springbootstudy.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor //final이 붙거나, @NotNull 이 붙은 필드의 생성자를 추가하는 어노테이션
+@Service //빈으로 등록
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    //블로그 글 추가 메서드
+    public Article save(AddArticleRequest articleRequest) {
+        return  blogRepository.save(articleRequest.toEntity()); //data를 entity로 변환해서 레파지토리에 저장하는 메서드
+    }
+}
