a9e8c77952e17376b3fdae746d80d939ea9cf6f8
choahra <ahar0729@gamil.com>
2023-05-21 11:32:54 +0900

    search api add

--- src/main/java/com/example/springbootstudy/controller/BlogApiController.java
+++ src/main/java/com/example/springbootstudy/controller/BlogApiController.java
@@ -1,14 +1,19 @@
 package com.example.springbootstudy.controller;
 
 import com.example.springbootstudy.dto.AddArticleRequest;
+import com.example.springbootstudy.dto.ArticleResponse;
 import com.example.springbootstudy.entity.Article;
 import com.example.springbootstudy.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +26,16 @@ public class BlogApiController {
         Article savedArticle = blogService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle); //요청한 자원이 성공적으로 생성되고, 저장된 블로그 글 정보를 응답 객체에 담아 전송
+    }
+
+    @GetMapping("api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
--- src/main/java/com/example/springbootstudy/service/BlogService.java
+++ src/main/java/com/example/springbootstudy/service/BlogService.java
@@ -6,6 +6,8 @@ import com.example.springbootstudy.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor //final이 붙거나, @NotNull 이 붙은 필드의 생성자를 추가하는 어노테이션
 @Service //빈으로 등록
 public class BlogService {
@@ -15,5 +17,9 @@ public class BlogService {
     //블로그 글 추가 메서드
     public Article save(AddArticleRequest articleRequest) {
         return  blogRepository.save(articleRequest.toEntity()); //data를 entity로 변환해서 레파지토리에 저장하는 메서드
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
